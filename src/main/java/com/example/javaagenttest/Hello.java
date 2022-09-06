98db29d8acc789a0ddb1274f7ee592fa7b0cad11
zhouchao@mireal-tech.cn <zc1063420481>
2022-09-06 15:01:18 +0800

    提交修改

--- src/main/java/com/example/javaagenttest/Hello.java
+++ src/main/java/com/example/javaagenttest/Hello.java
@@ -2,16 +2,13 @@ package com.example.javaagenttest;
 
 /**
  * @author ：周超
- * @date ：2022/9/5  15:18
  */
 public class Hello {
 
 	/**
 	 * 描述：测试
 	 *
-	 * @return void
 	 * @author 周超
-	 * @date 2022/9/6 14:22
 	 */
 	public void test() {
 		System.out.println("test11111");
--- src/main/java/com/example/javaagenttest/RestController.java
+++ src/main/java/com/example/javaagenttest/RestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author ：周超
- * @date ：2022/9/5  16:43
  */
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/test")
@@ -15,7 +14,6 @@ public class RestController {
 	 * 描述： 测试方法
 	 *
 	 * @author 周超
-	 * @date 2022/9/6 14:22
 	 */
 	@RequestMapping("/test")
 	public void test() {
